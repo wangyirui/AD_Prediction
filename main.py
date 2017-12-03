36ddab289f5399ac89c99cf085b2806f25392af6
wangyirui <yiruiwang06@gmail.com>
2017-12-02 22:25:45 -0500

    add information print during training and validation

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-from sklearn.preprocessing import OneHotEncoder
-
 from custom_transform import CustomResize
 
 from AD_Dataset import AD_Dataset
@@ -115,8 +113,6 @@ def main(options):
 
         optimizer = eval("torch.optim." + options.optimizer)(model.parameters(), options.learning_rate)
 
-        onehot_encoder = OneHotEncoder(sparse=False)
-
         # Prepare for label encoding
         last_dev_avg_loss = float("inf")
         best_accuracy = float("-inf")
@@ -151,16 +147,16 @@ def main(options):
                 correct_this_batch = (predict.squeeze(1) == ground_truth).sum()
                 correct_cnt += correct_this_batch
                 accuracy = float(correct_this_batch) / len(ground_truth)
-                logging.debug("loss at batch {0}: {1}".format(it, loss.data[0]))
-                logging.debug("accuracy at batch {0}: {1}".format(it, accuracy))
+                logging.info("loss at batch {0}: {1}".format(it, loss.data[0]))
+                logging.info("accuracy at batch {0}: {1}".format(it, accuracy))
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
             train_avg_loss = train_loss / (len(dset_train) / options.batch_size)
             train_avg_acu = float(correct_cnt) / len(dset_train)
-            logging.info("Average training loss is {0} at the end of epoch {1}".format(train_avg_loss.data[0], epoch_i))
-            logging.info("Average training accuracy is {0} at the end of epoch {1}".format(train_avg_acu, epoch_i))
+            logging.info("Average training loss is {0:.6f} at the end of epoch {1}".format(train_avg_loss.data[0], epoch_i))
+            logging.info("Average training accuracy is {0:.6f} at the end of epoch {1}".format(train_avg_acu, epoch_i))
             
             # validation -- this is a crude esitmation because there might be some paddings at the end
             dev_loss = 0.0
@@ -184,14 +180,18 @@ def main(options):
                 _, predict = test_prob_predict.topk(1)
                 loss = criterion(test_prob_loss, ground_truth)
                 dev_loss += loss
+                correct_this_batch = (predict.squeeze(1) == ground_truth).sum()
                 correct_cnt += (predict.squeeze(1) == ground_truth).sum()
+                accuracy = float(correct_this_batch) / len(ground_truth)
+                logging.info("loss at batch {0:.6f}: {1}".format(it, loss.data[0]))
+                logging.info("accuracy at batch {0:.6f}: {1}".format(it, accuracy))
 
             dev_avg_loss = dev_loss / (len(dset_test) / options.batch_size)
             dev_avg_acu = float(correct_cnt) / len(dset_test)
-            logging.info("Average validation loss is {0} at the end of epoch {1}".format(dev_avg_loss.data[0], epoch_i))
-            logging.info("Average validation accuracy is {0} at the end of epoch {1}".format(dev_avg_acu, epoch_i))
+            logging.info("Average validation loss is {0:.6f} at the end of epoch {1}".format(dev_avg_loss.data[0], epoch_i))
+            logging.info("Average validation accuracy is {0:.6f} at the end of epoch {1}".format(dev_avg_acu, epoch_i))
 
-            torch.save(model.state_dict(), open(options.save + ".nll_{0:.2f}.epoch_{1}".format(dev_avg_loss.data[0], epoch_i), 'wb'))
+            torch.save(model.state_dict(), open(options.save + ".nll_{0:.4f}.epoch_{1}".format(dev_avg_loss.data[0], epoch_i), 'wb'))
 
             last_dev_avg_loss = dev_avg_loss
 
